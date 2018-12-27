e186cf8e48fc7dfcf2e8beda2c3ec619c1e085c8
VladKishkurno <rmsi12345@gmail.com>
2018-12-27 18:45:27 +0300

    Add project files.

---
+++ Arrays-String/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+
+        }
+    }
+}
